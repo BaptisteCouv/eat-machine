2b1ab4bdbfec0a7177ff62558697641eeae5396c
BaptisteC <bcouavoux@positivethinking.tech>
2024-04-06 00:01:33 +0200

    ajout de la partie authentification

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,42 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    path: "/",
+    component: () => import("@/layouts/default/Default.vue"),
     children: [
       {
-        path: '/',
-        name: 'Home',
+        path: "/",
+        name: "Home",
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
       },
       {
-        path: '/listFood',
-        name: 'listFood',
-        component: () => import('@/views/ListFood.vue')
+        path: "/listFood",
+        name: "listFood",
+        component: () => import("@/views/ListFood.vue"),
       },
       {
-        path: '/listMeal',
-        name: 'listMeal',
-        component: () => import('@/views/ListMeal.vue')
+        path: "/listMeal",
+        name: "listMeal",
+        component: () => import("@/views/ListMeal.vue"),
       },
     ],
   },
-]
+  {
+    path: "/auth",
+    component: () => import("@/components/login/FirstPageConnection.vue"),
+    children: [],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
