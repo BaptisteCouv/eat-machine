ac03c3cc583ba2edb0dc7ff6e5052ce78000390b
BaptisteC <bcouavoux@positivethinking.tech>
2024-04-24 18:06:59 +0200

    ajout de l'authenntification 'token'

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,16 +1,22 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from "axios";
 
 // Configurer l'en-tête 'Access-Control-Allow-Origin' pour autoriser les requêtes depuis un domaine spécifique
 // axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'https://localhost:3000/';
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
 // Vous pouvez également configurer d'autres en-têtes CORS au besoin
-axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
-axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Content-Type';
+axios.defaults.headers.common["Access-Control-Allow-Methods"] =
+  "GET, POST, PUT, DELETE";
+axios.defaults.headers.common["Access-Control-Allow-Headers"] = "Content-Type";
 
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/', // L'URL API
+
+const token = localStorage.getItem("token");
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: "http://localhost:8080/", // L'URL API
+  headers: { Authorization: `Bearer ${token}` },
   // baseURL: 'https://eat-machine-back-prod-b5194754aa44.herokuapp.com/', // L'URL API
 });
+
 
 export default axiosInstance;
